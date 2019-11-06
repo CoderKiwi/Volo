d7af8ec794cbfc728a695949f7baace98817d56b
CoderKiwi <33564041+CoderKiwi@users.noreply.github.com>
2019-11-07 00:02:24 +1300

    Add nav bar with app title and 'Discover' tab
    - import bootstrap-vue

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,17 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import Home from './views/Home.vue';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-  ],
+export default new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+    ],
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
-    <HelloWorld msg="Welcome to Volo"/>
-  </div>
+    <div class="home">
+        <NavigationBar/>
+        <!--        <img alt="Vue logo" src="../assets/logo.png">-->
+        <!--        <HelloWorld msg="Welcome to Volo"/>-->
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+    import {Component, Vue} from "vue-property-decorator";
+    import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+    import NavigationBar from "@/components/NavigationBar.vue";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+    @Component({
+        components: {
+            HelloWorld,
+            NavigationBar,
+        },
+    })
+    export default class Home extends Vue {
+    }
 </script>
