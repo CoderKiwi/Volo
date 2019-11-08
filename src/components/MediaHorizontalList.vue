43230375f31f7fcc8485d49b2145045b069e96d7
CoderKiwi <33564041+CoderKiwi@users.noreply.github.com>
2019-11-08 16:47:39 +1300

    Add MediaHorizontalList showing horizontally-scrollable tiles to Home

---
+++ src/components/MediaHorizontalList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-container class="container">
+            <MediaTile title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+            <MediaTile title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+            <MediaTile title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+            <MediaTile title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+            <MediaTile title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+        </b-container>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import MediaTile from "@/components/MediaTile.vue";
+
+    @Component({
+        components: {
+            MediaTile,
+        },
+    })
+
+    export default class MediaHorizontalList extends Vue {
+    }
+</script>
+
+<style scoped>
+    .container {
+        display: flex;
+        overflow-x: auto;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,22 @@
     <div>
         <NavigationBar/>
         <h1>Trending Movies</h1>
-        <MediaTile class="tile" title="Braveheart" year="1995" img-source-uri="https://placekitten.com/380/200"/>
+        <MediaHorizontalList class="MediaHorizontalList"/>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import NavigationBar from '@/components/NavigationBar.vue';
-    import MediaTile from '@/components/MediaTile.vue';
+    import {Component, Vue} from "vue-property-decorator";
+    import NavigationBar from "@/components/NavigationBar.vue";
+    import MediaHorizontalList from "@/components/MediaHorizontalList.vue";
 
     @Component({
         components: {
             NavigationBar,
-            MediaTile,
+            MediaHorizontalList,
         },
     })
+
     export default class Home extends Vue {
     }
 </script>
