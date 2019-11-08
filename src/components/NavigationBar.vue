8b4df789cb512aedceadbc1baec20fda325b9349
CoderKiwi <33564041+CoderKiwi@users.noreply.github.com>
2019-11-08 23:21:57 +1300

    Remove unnecessary 'Discover' tab in navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,20 +2,12 @@
     <div>
         <b-navbar toggleable="lg" type="dark" variant="dark">
             <b-navbar-brand>Volo</b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item href="#">Discover</b-nav-item>
-                </b-navbar-nav>
-            </b-collapse>
         </b-navbar>
     </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue} from "vue-property-decorator";
 
     @Component
     export default class NavigationBar extends Vue {
